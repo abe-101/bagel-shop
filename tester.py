022785a9d312ada48f5c4ecf0b5a743db540b8fe
Abe <abe@habet.dev>
2022-01-11 11:23:15 -0500

    change breakfast colunm to NULL

--- tester.py
+++ tester.py
@@ -7,5 +7,5 @@ uri = os.getenv("DATABASE_URL")
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://")
 db = SQL(uri)
-db.execute("CREATE TABLE users2 (id SERIAL PRIMARY KEY, username TEXT NOT NULL, hash TEXT NOT NULL, cash NUMERIC NOT NULL DEFAULT 10000.00)")
+db.execute("CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT NOT NULL, hash TEXT NOT NULL, breakfast TEXT NULL)")
 
