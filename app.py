8e86c524e358db92555a125bf2ca4e01141c0a20
Abe <abe@habet.dev>
2022-01-20 09:00:46 -0500

    update email time

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ if uri.startswith("postgres://"):
 db = SQL(uri)
 
 # interval example
-@scheduler.task('cron', id='do_job_1', week='*', day_of_week='thur', hour='9', minute='0')
+@scheduler.task('cron', id='do_job_1', week='*', day_of_week='thur', hour='9', minute='2')
 def job1():
     with scheduler.app.app_context():
         message = Message("test worked!", recipients=["app@habet.dev"])
